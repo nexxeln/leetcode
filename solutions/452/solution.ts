a746f451892523bae31d7dca0ad038d15b75f02d
Shoubhit Dash <shoubhit2005@gmail.com>
2023-03-30 14:44:09 +0530

    ^: remove comments

--- solutions/452/solution.ts
+++ solutions/452/solution.ts
@@ -1,13 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.181.0/testing/asserts.ts";
 
 function findMinArrowShots(points: number[][]): number {
-  // 1. Sort the points array based on the xend values in ascending order[1].
-  // 2. Initialize a variable count to store the number of arrows needed, and set it to 1.
-  // 3. Initialize a variable prev_end to store the end value of the previous balloon, and set it to the xend value of the first balloon in the sorted array.
-  // 4. Iterate through the sorted points array starting from the second balloon. For each balloon, check if its xstart value is greater than prev_end. If it is, increment count by 1 and update prev_end to the current balloon's xend value.
-  // 5. Return the count variable as the minimum number of arrows needed to burst all balloons[1].
-
-  // solve
   points.sort((a, b) => a[1] - b[1]);
 
   let count = 1;
