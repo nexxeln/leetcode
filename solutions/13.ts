2c15e0289f296e45ed6e5e88c2170b36724d81d5
Shoubhit Dash <shoubhit2005@gmail.com>
2023-07-20 14:41:19 +0530

    add 13

---
+++ solutions/13.ts
@@ -0,0 +1,30 @@
+type RomanChars = "I" | "V" | "X" | "L" | "C" | "D" | "M";
+
+export function romanToInt(s: string): number {
+  const romanMap: Record<RomanChars, number> = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  const arr = s.split("") as RomanChars[];
+
+  let int = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = romanMap[arr[i]];
+    const next = romanMap[arr[i + 1]];
+
+    if (current < next) {
+      int -= current;
+    } else {
+      int += current;
+    }
+  }
+
+  return int;
+}
